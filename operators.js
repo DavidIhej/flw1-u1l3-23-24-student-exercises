4bf2c0906f3e6cc04a8d39c1c6a9be31a950ed5d
DavidIhej <121175000+DavidIhej@users.noreply.github.com>
2023-10-24 22:25:15 +0000

    Yes Creatures Yes

--- operators.js
+++ operators.js
@@ -6,17 +6,24 @@
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
 
-
+let num1 = 2;
+let num2 = 3;
+let num3 = num1 + num2;
+console.log(num3);
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
 
+let num4 = num2 - num1;  
+console.log(num4);
+
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
+ let product = num1 * num2;
+console.log(product);
 
 
 // String Operations
@@ -26,3 +33,7 @@
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
 
+let str1 = "Hello";
+let str2 = "World";
+
+console.log(str1 + " " +  str2);
